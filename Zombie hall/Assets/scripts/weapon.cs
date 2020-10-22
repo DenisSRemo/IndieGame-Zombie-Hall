f0b896cd03bc0558a25f8a016932aff67122988e
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2020-10-22 16:51:52 +0100

    progress week 1

---
+++ Zombie hall/Assets/scripts/weapon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    private bool allow=true;
+    public float firerate = 0.1f;
+    private float timetoshoot = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if(Time.time-timetoshoot>=firerate)
+            {
+                Shoot();
+                timetoshoot = Time.time;
+            }
+           
+        }
+    }
+
+
+
+    void Shoot()
+    {
+        
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        
+    }
+}
