1a0d5f613e24b3905a8f54c40d5386e4096ae489
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2021-01-18 16:20:21 +0000

    commit

--- Zombie hall/Assets/scripts/ExitDoor.cs
+++ Zombie hall/Assets/scripts/ExitDoor.cs
@@ -7,14 +7,10 @@ public class ExitDoor : MonoBehaviour
     [SerializeField] private Transform A;
 
     [SerializeField] private player Player;
-    [SerializeField] private List<Collider2D> zombies;
-    public bool player_exited;
-    [SerializeField]private float timing;
+    
     void Start()
     {
-        player_exited = false;
-
-        timing = Time.time;
+       
     }
 
     // Update is called once per frame
@@ -57,18 +53,9 @@ public class ExitDoor : MonoBehaviour
 
         if (collision.tag == "Zombie")
         {
-            Zombie zombie = collision.gameObject.GetComponent<Zombie>();
+          
 
-
-            if (zombie.through_door)
-            {
-
-                Vector3 u = A.transform.position;
-                zombies[0].transform.position = u;
-                zombies.Remove(zombies[0]);
-                timing = Time.time;
-                zombie.through_door = false;
-            }
+          
         }
 
 
