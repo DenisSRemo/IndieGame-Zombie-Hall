824adeadd32e7a588331559af16241f25c211883
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2021-01-30 17:25:12 +0000

    commit

---
+++ Zombie hall/Assets/scripts/UI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+
+
+
+
+
+    public GameObject Image, Button, Button1, Image2,Object;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Image.SetActive(false);
+        Button.SetActive(false);
+        Image2.SetActive(false);
+        Button1.SetActive(false);
+        Object.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
