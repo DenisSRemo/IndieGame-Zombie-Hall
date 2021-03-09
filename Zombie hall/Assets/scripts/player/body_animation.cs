db53f2a8d15a8e9aaae8f90af544e1d47066aa49
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2021-03-09 16:13:34 +0000

    main character animation done

---
+++ Zombie hall/Assets/scripts/player/body_animation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class body_animation : MonoBehaviour
+{
+
+
+    [SerializeField] private Animator animator;
+    private bool walking;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        walking = false;
+        animator.SetBool("walking", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("a"))
+        {
+            walking = true;
+            animator.SetBool("walking", walking);
+        }
+        else
+        if (Input.GetKey("d"))
+        {
+            walking = true;
+            animator.SetBool("walking", walking);
+        }
+        else
+        {
+            walking = false;
+            animator.SetBool("walking", walking);
+        }
+    }
+}
