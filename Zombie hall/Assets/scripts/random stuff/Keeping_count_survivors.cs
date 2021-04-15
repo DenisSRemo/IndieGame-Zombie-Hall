d3f3b619bc72a2cc44a6643c4313e34c2f7459b1
DenisSRemo <43639388+DenisSRemo@users.noreply.github.com>
2021-04-15 15:29:42 +0100

    game extension

---
+++ Zombie hall/Assets/scripts/random stuff/Keeping_count_survivors.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keeping_count_survivors : MonoBehaviour
+{
+
+
+    public static bool survivor1, survivor2, survivor3, survivor4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        survivor1 = false;
+        survivor2 = false;
+        survivor3 = false;
+        survivor4 = false;
+    }
+
+   
+}
